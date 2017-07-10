a2560402a7031adfe0b0f414b230254b70494e43
Raj <rajveer.singh@wsu.edu>
2017-07-10 00:04:58 -0700

    Update scroll.js

--- public/src/js/directives/scroll.js
+++ public/src/js/directives/scroll.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ark_explorer')
+angular.module('bpl-explorer')
   .directive('scroll', function ($window) {
       return function (scope, element, attrs) {
           angular.element($window).bind('scroll', function () {
