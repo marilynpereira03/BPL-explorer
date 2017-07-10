891a152ea179bc9ea45750584969f4c802a191d2
Raj <rajveer.singh@wsu.edu>
2017-07-10 00:00:15 -0700

    Update currency.js

--- public/src/js/controllers/currency.js
+++ public/src/js/controllers/currency.js
@@ -1,13 +1,13 @@
 'use strict';
 
-angular.module ('ark_explorer.currency').controller ('CurrencyController',
+angular.module ('bpl-explorer.currency').controller ('CurrencyController',
   function ($scope, $rootScope) {
-    $rootScope.currency.symbol = localStorage && localStorage.getItem ('ark_explorer-currency') || 'ARK';
+    $rootScope.currency.symbol = localStorage && localStorage.getItem ('bpl-explorer-currency') || 'BPL';
 
     $scope.setCurrency = function(currency) {
       $rootScope.currency.symbol = currency;
       if (localStorage) {
-        localStorage.setItem ('ark_explorer-currency', currency);
+        localStorage.setItem ('bpl-explorer-currency', currency);
       }
     };
   });
