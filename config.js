a6a788ee9593994d715ec799c19c85333b929be0
Raj <rajveer.singh@wsu.edu>
2017-07-09 23:10:39 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ var config = require('./config.global');
 // CONFIGURATION
 // =======================================================================================
 
-config.host = '0.0.0.0'; // Interface to listen on, 0.0.0.0 to listen on all available
-config.port = 6040;      // Port to listen on
+config.host = '127.0.0.'; // Interface to listen on, 0.0.0.0 to listen on all available
+config.port = 4001;      // Port to listen on
 
-// ARK node
-config.ark.host = '127.0.0.1';
-config.ark.port = 4001;
+// BPL node
+config.bpl.host = '127.0.0.1';
+config.bpl.port = 4000;
 
 // FreeGeoIP server
 config.freegeoip.host = '127.0.0.1';
@@ -24,11 +24,11 @@ config.redis.password = '';
 config.cacheTTL = 20; // Time in seconds to store cache in Redis
 
 // Header price tickers, Currency switcher
-config.exchangeRates.enabled = true;         // Exchange rates support (true - enabled, false - disabled)
+config.exchangeRates.enabled = false;         // Exchange rates support (true - enabled, false - disabled)
 config.exchangeRates.updateInterval = 30000; // Interval in ms for checking exchange rates (default: 30 seconds)
 // Configuration for different currency pairs, set false to disable pair
-config.exchangeRates.exchanges.ARK.BTC = 'bittrex';   // ARK/BTC pair, supported: bittrex
-config.exchangeRates.exchanges.ARK.CNY = false;      // ARK/CNY pair, supported: jubi, bitbays
+config.exchangeRates.exchanges.BPL.BTC = 'poloniex';  // BPL/BTC pair, supported: poloniex
+config.exchangeRates.exchanges.BPL.CNY = 'jubi';      // BPL/CNY pair, supported: jubi, bitbays
 config.exchangeRates.exchanges.BTC.USD = 'bitfinex';  // BTC/USD pair, supported: bitfinex, bitstamp, btce
 config.exchangeRates.exchanges.BTC.EUR = 'bitstamp';  // BTC/EUR pair, supported: bitstamp, bitmarket
 config.exchangeRates.exchanges.BTC.PLN = 'bitmarket'; // BTC/PLN pair, supported: bitmarket
