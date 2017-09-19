1c8b344e9d348dc82a83758e5f0565d1d93b9d8c
marilynpereira03 <marilyn.pereira@harbingergroup.com>
2017-09-19 16:16:13 +0530

    Changed config host.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = require('./config.global');
 // CONFIGURATION
 // =======================================================================================
 
-config.host = '13.56.163.57'; // Interface to listen on, 0.0.0.0 to listen on all available
+config.host = 'ec2-13-56-163-57.us-west-1.compute.amazonaws.com'; // Interface to listen on, 0.0.0.0 to listen on all available
 config.port = 4002;      // Port to listen on
 
 // BPL node
